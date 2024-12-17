0e7b1b7a1d20f56c479633dcd7584669e719c47c
temple_19 <tozoaz@gmail.com>
2024-12-17 19:59:29 +0100

    signin 2

--- app/get-started/page.jsx
+++ app/get-started/page.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
 const page = () => {
+  const [checkboxOneChecked, setCheckboxOneChecked] = useState(false);
+  const [checkboxTwoChecked, setCheckboxTwoChecked] = useState(false);
+
+  const handleCheckboxChange = (event, setState) => {
+    setState(event.target.checked);
+  };
+
   return (
-    <div className="bg-white rounded-xl p-4 mx-auto w-60% ">
+    <div className="bg-white rounded-xl p-4 mx-auto max-w-[650px] ">
       <h2 className="text-[36px] mb-5">
         Sign up to connect with the right brands
       </h2>
@@ -21,18 +29,40 @@ const page = () => {
       </div>
       <div className="">
         <div className="space-x-6 flex justify-between">
-          <input className=" border w-[40%] my-2 rounded-xl border-[#E9ECE9]" />
-          <input className=" border w-[40%] my-2 rounded-xl border-[#E9ECE9]" />
+          <input className=" border w-[50%] my-2 rounded-xl p-2 border-[#E9ECE9]" />
+          <input className=" border w-[50%] my-2 rounded-xl p-2 border-[#E9ECE9]" />
         </div>
-        <input className=" w-full border  my-2 rounded-xl border-[#E9ECE9] " />
-        <input className=" w-full border  my-2 rounded-xl border-[#E9ECE9] " />
-        <input className=" w-full border  my-2 rounded-xl border-[#E9ECE9] " />
-        <p>Send me helpful emails to find rewarding work and job leads.</p>
-        <p>
-          Yes, I understand and agree to the TribeFind Terms of Service,
-          including the User Agreement and Privacy policy
-        </p>
-        <input className=" w-full border   my-2 rounded-xl border-[#E9ECE9]" />
+        <input className=" w-full border  my-2 rounded-xl p-2 border-[#E9ECE9] " />
+        <input className=" w-full border  my-2 rounded-xl p-2 border-[#E9ECE9] " />
+        <input className=" w-full border  my-2 rounded-xl p-2 border-[#E9ECE9] " />
+        {/* Checkbox 1 */}
+        <div className="flex items-start my-2">
+          <input
+            type="checkbox"
+            className="mt-1 mr-2"
+            onChange={(e) => handleCheckboxChange(e, setCheckboxOneChecked)}
+          />
+          <p>Send me helpful emails to find rewarding work and job leads.</p>
+        </div>
+
+        {/* Checkbox 2 */}
+        <div className="flex items-start my-2">
+          <input
+            type="checkbox"
+            className="mt-1 mr-2"
+            onChange={(e) => handleCheckboxChange(e, setCheckboxTwoChecked)}
+          />
+          <p>
+            Yes, I understand and agree to the TribeFind Terms of Service,
+            including the User Agreement and Privacy policy.
+          </p>
+        </div>
+        <button className="p-2 w-full mt-3 bg-[#FF501E] text-black font-semibold whitespace-nowrap rounded-lg border-2 border-r-4 border-b-4 border-black">
+          Create An Account
+        </button>
+        <button className="p-2 w-full mt-3 text-[14px] text-black font-semibold whitespace-nowrap rounded-lg ">
+          Already have an account? Log In
+        </button>
       </div>
     </div>
   );
